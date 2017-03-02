471c54ce8ebdb9b0a00508b87ccdfb75a38af46f
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-03-02 22:32:59 +0100

    update mall

--- html/index.php
+++ html/index.php
@@ -1,13 +1,60 @@
-<!doctype html>
+<?php
+//functions
+include "function/function.php";
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Digitalis Home</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+    <meta name="description" content="Digitalis"/>
+    <meta name="keywords" content="webdesing, creative design"/>
+    <meta name="author" content="Digital Internet Solutions">
 
+    <!--css-->
+    <link href="css/app.css" rel="stylesheet">
+
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900i" rel="stylesheet">
+
+    <!--fontawesome-->
+    <script src="https://use.fontawesome.com/28b7055de1.js"></script>
+
+    <!--JQuery-->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <!--Velocity -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.4.3/velocity.min.js" type="text/javascript"></script>
+
+    <!--all js minified-->
+    <script src="js/app.js"></script>
+
+    <!-- Latest compiled and minified JavaScript for bootstrap-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+            crossorigin="anonymous"></script>
+
+    <!--icon-->
+    <link rel="icon" href="img/logo/favicon.png">
+</head>
+<body class="wrapper col-xs-12" id="page-top">
+<?php
+// include nav and language page
+include "include_pages/nav.php";
+?>
+
+<header>
+
+</header>
+
+<section>
+
+</section>
+
+<?php
+include "include_pages/footer.php";
+?>
 </body>
 </html>
