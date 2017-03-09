69414eeec17e2a45b04c90a9996dc573884d933a
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-03-09 19:24:23 +0100

    fixar saker

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //import everything
-var gulp        = require('gulp');
+var g           = require('gulp');
 var sass        = require('gulp-sass');
 var cleanCss    = require('gulp-clean-css');
 var prefix      = require('gulp-autoprefixer');
@@ -10,79 +10,87 @@ var imgmin      = require('gulp-imagemin');
 var watch       = require('gulp-watch');
 var plumber     = require('gulp-plumber');
 var removeComm  = require('gulp-remove-html-comments');
+var clean       = require('gulp-clean');
+
+//delete dist folder
+g.task('delete', function () {
+    return g.src('dist', {read: false})
+        .pipe(clean());
+});
 
 //remove comments from php and html documents
-gulp.task('removeComm', function () {
-    return gulp.src('app/**/*.php')
+g.task('removeComm', function () {
+    return g.src('app/**/*.php')
         .pipe(removeComm())
-        .pipe(gulp.dest('dist/'));
+        .pipe(g.dest('dist/'));
 });
 
 //minify html and php files
-gulp.task('minify', ['removeComm'] , function() {
-    return gulp.src(['app/**/*.php' ,'app/**/*.html'])
+g.task('minify', ['removeComm'] , function() {
+    return g.src(['app/**/*.php' ,'app/**/*.html'])
         .pipe(plumber())
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+        .pipe(g.dest('dist'));
 });
 
 //convert scss to css and add prefixes
-gulp.task('sass', function () {
-    return gulp.src('app/scss/app.scss')
+g.task('sass', function () {
+    return g.src('app/scss/app.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(prefix('last 2 versions'))
-        .pipe(gulp.dest('app/css'));
+        .pipe(g.dest('app/css'));
 });
 
 //minify css files and run the sass function before
-gulp.task('minify-css', ['sass'], function() {
-    return gulp.src('app/css/*.css')
+g.task('minify-css', ['sass'], function() {
+    return g.src('app/css/*.css')
         .pipe(plumber())
         .pipe(cleanCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(g.dest('dist/css'));
 });
 
 //uglify and concat some files
-gulp.task('scripts', function () {
-    return gulp.src(['app/js/app.js'])
+g.task('scripts', function () {
+    return g.src(['app/js/app.js'])
         .pipe(plumber())
         .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(g.dest('dist/js'))
 });
 
 //uglify all flies
-gulp.task('scripts', function () {
-    return gulp.src('app/js/*.js')
+g.task('scripts', function () {
+    return g.src('app/js/*.js')
         .pipe(plumber())
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(g.dest('dist/js'))
 });
 
-//coppy img and .htaccess files to dist folder
-gulp.task('copy', function() {
-    return gulp.src('app/.htaccess')
+//copy img and .htaccess files to dist folder
+g.task('copy', function() {
+    return g.src('app/.htaccess')
         .pipe(plumber())
-        .pipe(gulp.dest('dist'));
+        .pipe(g.dest('dist'));
 });
 
 //compress all images from app folder
-gulp.task('imgmin', function () {
-    return gulp.src('app/img/**/*')
+g.task('imgmin', function () {
+    return g.src('app/img/**/*')
         .pipe(plumber())
         .pipe(imgmin())
-        .pipe(gulp.dest('dist/img'));
+        .pipe(g.dest('dist/img'));
 });
 
 // watch for file changes and performs the different tasks
-gulp.task('watch', function () {
-    gulp.watch('app/js/*js',         ['scripts']);
-    gulp.watch('app/scss/*scss',     ['minify-css']);
-    gulp.watch('app/**/*{html,php}', ['minify']);
-    gulp.watch('app/img/**/*',       ['imgmin']);
-    gulp.watch('app/.htaccess',      ['copy']);
+g.task('watch', function () {
+    g.watch('app/js/*js',         ['scripts']);
+    g.watch('app/scss/*scss',     ['minify-css']);
+    g.watch('app/**/*{html,php}', ['minify']);
+    g.watch('app/img/**/*',       ['imgmin']);
+    g.watch('app/.htaccess',      ['copy']);
 });
 
-// Run everything
-gulp.task('default', ['minify-css', 'minify', 'scripts', 'imgmin', 'copy', 'watch']);
+g.task('build', ['minify-css', 'minify', 'scripts', 'imgmin', 'copy', 'watch']);
+
+g.task('default', ['build']);
