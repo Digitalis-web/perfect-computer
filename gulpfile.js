9a1ccd7cd3b46d3498502e15668d1fcda27474d1
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-03-20 14:38:01 +0100

    mall update with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,36 @@
 //import everything
-var g           = require('gulp');
-var sass        = require('gulp-sass');
-var cleanCss    = require('gulp-clean-css');
-var prefix      = require('gulp-autoprefixer');
-var uglify      = require('gulp-uglify');
-var concat      = require('gulp-concat');
-var htmlmin     = require('gulp-htmlmin');
-var imgmin      = require('gulp-imagemin');
-var watch       = require('gulp-watch');
-var plumber     = require('gulp-plumber');
-var removeComm  = require('gulp-remove-html-comments');
-var clean       = require('gulp-clean');
+var g               = require('gulp');
+var sass            = require('gulp-sass');
+var cleanCss        = require('gulp-clean-css');
+var prefix          = require('gulp-autoprefixer');
+var uglify          = require('gulp-uglify');
+var concat          = require('gulp-concat');
+var htmlmin         = require('gulp-htmlmin');
+var imgmin          = require('gulp-imagemin');
+var watch           = require('gulp-watch');
+var plumber         = require('gulp-plumber');
+var removeComm      = require('gulp-remove-html-comments');
+var clean           = require('gulp-clean');
+var httpProxy       = require('http-proxy');
+var browserSync     = require('browser-sync');
+var connect         = require('gulp-connect-php');
 
 //delete dist folder
-g.task('delete', function () {
+g.task('clean', function () {
     return g.src('dist', {read: false})
         .pipe(clean());
 });
 
 //remove comments from php and html documents
 g.task('removeComm', function () {
-    return g.src('app/**/*.php')
+    return g.src(['app/**/*.php','app/**/*.html'])
+        .pipe(plumber())
         .pipe(removeComm())
         .pipe(g.dest('dist/'));
 });
 
 //minify html and php files
-g.task('minify', ['removeComm'] , function() {
+g.task('minify', ['removeComm'] , function(){
     return g.src(['app/**/*.php' ,'app/**/*.html'])
         .pipe(plumber())
         .pipe(htmlmin({collapseWhitespace: true}))
@@ -34,7 +38,7 @@ g.task('minify', ['removeComm'] , function() {
 });
 
 //convert scss to css and add prefixes
-g.task('sass', function () {
+g.task('compile-sass', function () {
     return g.src('app/scss/app.scss')
         .pipe(plumber())
         .pipe(sass())
@@ -43,24 +47,31 @@ g.task('sass', function () {
 });
 
 //minify css files and run the sass function before
-g.task('minify-css', ['sass'], function() {
+g.task('css-build', ['compile-sass'], function() {
     return g.src('app/css/*.css')
         .pipe(plumber())
         .pipe(cleanCss({compatibility: 'ie8'}))
         .pipe(g.dest('dist/css'));
 });
 
-//uglify and concat some files
-g.task('scripts', function () {
-    return g.src(['app/js/app.js'])
+//creating app.js from all js files in the app folder
+g.task('concat-js',function () {
+    return g.src('app/js/app/*.js)')
         .pipe(plumber())
         .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(g.dest('dist/js'))
+        .pipe(g.dest('app/js'))
+});
+
+//create impots.js from all js files in imporst folder
+g.task('concat-js',function () {
+    return g.src('app/js/imports/*.js')
+        .pipe(plumber())
+        .pipe(concat('import.js'))
+        .pipe(g.dest('app/js'))
 });
 
 //uglify all flies
-g.task('scripts', function () {
+g.task('js-build', ['concat-js','concat-js'] , function () {
     return g.src('app/js/*.js')
         .pipe(plumber())
         .pipe(uglify())
@@ -69,7 +80,7 @@ g.task('scripts', function () {
 
 //copy img and .htaccess files to dist folder
 g.task('copy', function() {
-    return g.src('app/.htaccess')
+    return g.src(['app/.htaccess', 'app/config.ini'])
         .pipe(plumber())
         .pipe(g.dest('dist'));
 });
@@ -82,15 +93,49 @@ g.task('imgmin', function () {
         .pipe(g.dest('dist/img'));
 });
 
-// watch for file changes and performs the different tasks
-g.task('watch', function () {
-    g.watch('app/js/*js',         ['scripts']);
-    g.watch('app/scss/*scss',     ['minify-css']);
-    g.watch('app/**/*{html,php}', ['minify']);
-    g.watch('app/img/**/*',       ['imgmin']);
-    g.watch('app/.htaccess',      ['copy']);
+g.task('connect-php', function () {
+    connect.server({
+        port: 8079,
+        base: 'app',
+        open: false
+    });
+
+    var proxy   = httpProxy.createProxyServer({});
+    var reload  = browserSync.reload;
+
+    browserSync({
+        notify: false,
+        port  : 8079,
+        server: {
+            baseDir   : ['app'],
+            middleware: function (req, res, next) {
+                var url = req.url;
+
+                if (!url.match(/^\/(css)\//)) {
+                    proxy.web(req, res, { target: 'http://localhost:8079' });
+                } else {
+                    next();
+                }
+            }
+        }
+    });
+
+    g.watch([
+        'app/*.html',
+        'app/*.php',
+        'app/js/*.js',
+        'app/css/app.css',
+        'app/img/**/*'
+    ]).on('change', reload);
+
+    g.watch('app/scss/*scss',     ['compile-sass']);
+    g.watch('app/js/**/*.js',     ['concat-js']);
+
 });
 
-g.task('build', ['minify-css', 'minify', 'scripts', 'imgmin', 'copy', 'watch']);
+g.task('build',['clean'],function () {
+    g.start('minify', 'css-build', 'js-build', 'copy', 'imgmin')
+});
 
-g.task('default', ['build']);
+//run css tole to compile css
+g.task('default', ['compile-sass', 'concat-js', 'connect-php']);
